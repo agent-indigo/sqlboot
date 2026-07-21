21842bd6975f9c421b78ba23c0b8de2a614c27cf
agent-indigo <bradenhynes@gmail.com>
2026-07-21 12:18:19 -0230

    Added Nonnull annotations

--- src/main/java/com/keyin/hynes/braden/sqlboot/entities/Listing.java
+++ src/main/java/com/keyin/hynes/braden/sqlboot/entities/Listing.java
@@ -1,29 +1,46 @@
 package com.keyin.hynes.braden.sqlboot.entities;
 import com.keyin.hynes.braden.sqlboot.abstracts.EntityBase;
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 @Data
 @Entity
 @EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@RequiredArgsConstructor
 @Table(name = "listings")
 @ToString(callSuper = true)
 public final class Listing extends EntityBase {
+  @Nonnull
   private String title;
+  @Nonnull
   private String address;
+  @Nonnull
   private String city;
+  @Nonnull
   private String state;
+  @Nonnull
   private String zipcode;
   private String description;
+  @Nonnull
   private Float price;
+  @Nonnull
   private Integer bedrooms;
+  @Nonnull
   private Float bathrooms;
+  @Nonnull
   private Integer garage;
+  @Nonnull
   private Float sqft;
+  @Nonnull
   private Float lot_size;
+  @Nonnull
   private String exterior_photo;
   private String interior_photo_1;
   private String interior_photo_2;
@@ -31,7 +48,9 @@ public final class Listing extends EntityBase {
   private String interior_photo_4;
   private String interior_photo_5;
   private String interior_photo_6;
+  @Nonnull
   private Boolean is_published;
   @ManyToOne
+  @Nonnull
   private Realtor realtor;
 }
--- src/main/java/com/keyin/hynes/braden/sqlboot/entities/Realtor.java
+++ src/main/java/com/keyin/hynes/braden/sqlboot/entities/Realtor.java
@@ -1,21 +1,32 @@
 package com.keyin.hynes.braden.sqlboot.entities;
 import com.keyin.hynes.braden.sqlboot.abstracts.EntityBase;
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 @Data
 @Entity
 @EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@RequiredArgsConstructor
 @Table(name = "realtors")
 @ToString(callSuper = true)
 public final class Realtor extends EntityBase {
+  @Nonnull
   private String first_name;
+  @Nonnull
   private String last_name;
+  @Nonnull
   private String photo;
   private String description;
+  @Nonnull
   private String email_address;
+  @Nonnull
   private String phone_number;
+  @Nonnull
   private Boolean is_mvp;
 }
